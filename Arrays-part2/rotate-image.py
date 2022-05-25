28bc36f18fdadfa2f338d92b63c0c60131c19e50
anand  chauhan <anandchauhan8791@gmail.com>
2022-05-25 12:54:53 +0530

    Create rotate-image.py

---
+++ Arrays-part2/rotate-image.py
@@ -0,0 +1,44 @@
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Example 2:
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+Constraints:
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+
+qustion link https://leetcode.com/problems/rotate-image/
+"""
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+
+        l, r = 0 , len(matrix) - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+                #save topLeft
+                topLeft = matrix[top][l+i]
+                #move bottem left to top left
+                matrix[top][l+i] = matrix[bottom - i][l]
+                #move bottpm right to bottom left
+                matrix[bottom - i][l] = matrix[bottom][r-i]
+                #move top righ to bottom right
+                matrix[bottom][r-i] = matrix[top + i][r]
+                #move top left to top right
+                matrix[top +i][r] = topLeft
+            r-=1
+            l+=1
+
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+x = Solution()
+x.rotate(matrix)
+print(matrix)
