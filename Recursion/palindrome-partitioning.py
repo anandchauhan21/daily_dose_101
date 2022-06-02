5c5fa694c9285d65deb1f1823434f57f36e09788
anand  chauhan <anandchauhan8791@gmail.com>
2022-06-02 23:57:14 +0530

    Update palindrome-partitioning.py

--- Recursion/palindrome-partitioning.py
+++ Recursion/palindrome-partitioning.py
@@ -23,3 +23,28 @@ qustion link https://leetcode.com/problems/palindrome-partitioning/
 from typing import List
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s,i,j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
+                    part.pop()
+        dfs(0)
+        return res
+    def isPali(self, s,l,r):
+        while l<r:
+            if s[l] != s[r]:
+                return False
+            l,r = l+1,r-1
+        return True
+x =Solution()
+s = "aab"
+print(x.partition(s))
+
+### out-put: [['a', 'a', 'b'], ['aa', 'b']]
