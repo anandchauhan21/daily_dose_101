e3ba72ec32fa3d4358db497152baa567ba9be651
anand  chauhan <anandchauhan8791@gmail.com>
2022-06-18 23:39:14 +0530

    Update clone-graph.py

--- Graph/clone-graph.py
+++ Graph/clone-graph.py
@@ -47,3 +47,23 @@ The Graph is connected and all nodes can be visited starting from the given node
 
 Question link https://leetcode.com/problems/clone-graph/
 """
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldToNew = {}
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        return dfs(node) if node else None
