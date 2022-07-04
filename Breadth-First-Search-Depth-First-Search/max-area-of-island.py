f8ab47349f2d6a23026290f26e5236bb8829b71b
ANAND <anandchauhan8791@gmail.com>
2022-07-04 16:19:45 +0530

    Create max-area-of-island.py

---
+++ Breadth-First-Search-Depth-First-Search/max-area-of-island.py
@@ -0,0 +1,62 @@
+"""
+max-area-of-island
+
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally
+(horizontal or vertical.) You may assume all four edges of the grid are surrounded by water. The area of an island is
+the number of cells with a value 1 in the island. Return the maximum area of an island in grid. If there is no
+island, return 0.
+
+Example 1:
+Input:
+grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+Question link https://leetcode.com/problems/max-area-of-island/
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or
+                    grid[r][c] == 0 or (r, c) in visit):
+                return 0
+            visit.add((r, c))
+            return (1 + dfs(r + 1, c) +
+                    dfs(r - 1, c) +
+                    dfs(r, c + 1) +
+                    dfs(r, c - 1))
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
+
+
+grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+
+x = Solution()
+print(x.maxAreaOfIsland(grid))
