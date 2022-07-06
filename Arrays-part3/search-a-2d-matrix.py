547df7045c0a753b462b2cf29a6308a492ea8776
ANAND <anandchauhan8791@gmail.com>
2022-07-06 12:33:36 +0530

    💫 fix some space

--- Arrays-part3/search-a-2d-matrix.py
+++ Arrays-part3/search-a-2d-matrix.py
@@ -22,35 +22,36 @@ n == matrix[i].length
 
 qustion link https://leetcode.com/problems/search-a-2d-matrix/
 """
+
+
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        ROWS,COLS = len(matrix),len(matrix[0])
-        top, bot = 0,ROWS-1
-        while top<=bot:
-            row = (top+bot)//2
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
             if target > matrix[row][-1]:
-                top = row+1
+                top = row + 1
             elif target < matrix[row][0]:
-                bot = row-1
+                bot = row - 1
             else:
                 break
-        if not (top<=bot):
+        if not (top <= bot):
             return False
-        row = (top+bot)//2
-        l,r =0,COLS-1
-        while l<=r:
-            m = (l+r)//2
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
             if target > matrix[row][m]:
-                l = m+1
+                l = m + 1
             elif target < matrix[row][m]:
-                r = m-1
+                r = m - 1
             else:
                 return True
         return False
+
+
 x = Solution()
-matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
 target = 3
-print(x.searchMatrix(matrix,target))
-
-
-
+print(x.searchMatrix(matrix, target))
