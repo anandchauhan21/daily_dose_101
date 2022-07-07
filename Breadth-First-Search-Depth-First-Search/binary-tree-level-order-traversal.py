ed56295a84282dea7c95261fe8e2d36878915047
ANAND <anandchauhan8791@gmail.com>
2022-07-07 15:05:02 +0530

    🔨 Create binary-tree-level-order-traversal.py

---
+++ Breadth-First-Search-Depth-First-Search/binary-tree-level-order-traversal.py
@@ -0,0 +1,47 @@
+"""
+binary-tree-level-order-traversal
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+
+Question link https://leetcode.com/problems/binary-tree-level-order-traversal/
+"""
+
+# Definition for a binary tree node.
+from collections import defaultdict
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d = defaultdict(list)
+
+        def dfs(node, level):
+            if not node: return
+            d[level].append(node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        return d.values()
