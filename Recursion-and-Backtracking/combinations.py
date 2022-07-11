bafcd867cf6f98f5a7915c4460e8224457b4c48e
ANAND <anandchauhan8791@gmail.com>
2022-07-11 12:00:50 +0530

    🔨 create combinations.py

---
+++ Recursion-and-Backtracking/combinations.py
@@ -0,0 +1,48 @@
+"""
+combinations
+Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+
+You may return the answer in any order.
+
+Example 1:
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+Example 2:
+Input: n = 1, k = 1
+Output: [[1]]
+
+Constraints:
+1 <= n <= 20
+1 <= k <= n
+
+Question link https://leetcode.com/problems/combinations/
+"""
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, comb):
+            if len(comb) == k:
+                res.append(comb.copy())
+                return
+            for i in range(start, n + 1):
+                comb.append(i)
+                backtrack(i + 1, comb)
+                comb.pop()
+
+        backtrack(1, [])
+        return res
+x =Solution()
+print(x.combine(4,2))
