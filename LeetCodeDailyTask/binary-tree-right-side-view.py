e65b019e2e1bf84292f6a5a1515ec516f0467ff7
ANAND <anandchauhan8791@gmail.com>
2022-07-11 11:20:13 +0530

    🔨 Create binary-tree-right-side-view.py

---
+++ LeetCodeDailyTask/binary-tree-right-side-view.py
@@ -0,0 +1,51 @@
+"""
+binary-tree-right-side-view
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example 1:
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+
+Example 2:
+Input: root = [1,null,3]
+Output: [1,3]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+Question link https://leetcode.com/problems/binary-tree-right-side-view/
+"""
+
+
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque([root])
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide =node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
