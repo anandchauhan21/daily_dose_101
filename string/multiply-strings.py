718b47bdf3bb407e7fa5c7fcd0a02da2438edbfd
ANAND <anandchauhan8791@gmail.com>
2022-07-14 13:11:07 +0530

    🔨 create multiply-strings.py

---
+++ string/multiply-strings.py
@@ -0,0 +1,47 @@
+"""
+multiply-strings
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+Example 1:
+Input: num1 = "2", num2 = "3"
+Output: "6"
+
+Example 2:
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+
+Constraints:
+1 <= num1.length, num2.length <= 200
+num1 and num2 consist of digits only.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+
+Question link https://leetcode.com/problems/multiply-strings/
+
+Time: O(nxm)
+Space:0(n+m)
+"""
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+        res = [0] * (len(num2) + len(num1))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += (res[i1 + i2] // 10)
+                res[i1 + i2] = res[i1 + i2] % 10
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        res = map(str, res[beg:])
+        return "".join(res)
+
+
+x = Solution()
+print(x.multiply("123", "456"))
