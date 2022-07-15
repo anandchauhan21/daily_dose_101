b6cf77f5501766017a649fd09c61ca4c7dcb917d
ANAND <anandchauhan8791@gmail.com>
2022-07-15 12:33:51 +0530

    🔨 create number-of-steps-to-reduce-a-number-to-zero.py

--- LeetCodeDailyTask/fizz-buzz.py
+++ LeetCodeDailyTask/fizz-buzz.py
@@ -28,7 +28,7 @@ from typing import List
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        res = [None] * n
+        res = [None]*n
         for i in range(1, n + 1):
             if i % 15 == 0:
                 res[i - 1] = 'FizzBuzz'
