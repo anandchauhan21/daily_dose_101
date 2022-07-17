58dbf0b787861da6251298be6615cf02c7f0b985
ANAND <anandchauhan8791@gmail.com>
2022-07-17 13:49:32 +0530

    📦 update intersection-of-two-arrays-ii.py

--- Arrays/intersection-of-two-arrays-ii.py
+++ Arrays/intersection-of-two-arrays-ii.py
@@ -35,3 +35,26 @@ class Solution:
 
 x = Solution()
 print(x.intersect([1, 2, 2, 1], [2, 2]))
+
+#####
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d1, d2 = {}, {}
+        for i in nums1:
+            if i not in d1:
+                d1[i] = 1
+            else:
+                d1[i] += 1
+        for i in nums2:
+            if i not in d2:
+                d2[i] = 1
+            else:
+                d2[i] += 1
+        res = []
+        # print(d1, d2)
+        for i in d1:
+            if i in d2:
+                res += [i] * min(d2[i], d1[i])
+        return res
