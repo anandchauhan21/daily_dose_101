cf1ac3f63b5c35e44bc957ad98dbb83dbb6024bb
ANAND <anandchauhan8791@gmail.com>
2022-07-20 10:04:46 +0530

    🔨 create number-of-matching-subsequences.py

--- test.pu.py
+++ test.pu.py
@@ -1,5 +1,11 @@
-n = 3
-k = 1
-memo = [[0] * (k + 1) for _ in range(n + 1)]
-
-print(memo)
+def bs(lst, i):
+    l, r = 0, len(lst)
+    print(type(l))
+    while l < r:
+        mid = (l + r) // 2
+        if i < lst[mid]:
+            r = [mid]
+        else:
+            l = mid + 1
+    return l
+x = bs(["a", "bb", "acd"], 1)
