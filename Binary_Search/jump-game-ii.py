4afbed8dea1fe9a552f06a128945651353ec6e44
ANAND <anandchauhan8791@gmail.com>
2022-07-20 12:11:59 +0530

    🔨 create jump-game-ii.py.py

---
+++ Binary_Search/jump-game-ii.py
@@ -0,0 +1,42 @@
+"""
+jump-game-ii
+
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Your goal is to reach the last index in the minimum number of jumps.
+You can assume that you can always reach the last index.
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [2,3,0,1,4]
+Output: 2
+
+Constraints:
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+
+Question link https://leetcode.com/problems/jump-game-ii/
+"""
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = 0
+        l = r = 0
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
+
+
+x = Solution()
+print(x.jump([2, 3, 1, 1, 4]))
