9f8be24f2167d4f6779324478fbb8e495868e7f0
ANAND <anandchauhan8791@gmail.com>
2022-07-21 11:41:43 +0530

    🔨 create reverse-linked-list-ii.py

---
+++ LeetCodeDailyTask/reverse-linked-list-ii.py
@@ -0,0 +1,52 @@
+"""
+reverse-linked-list-ii
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+
+Question link https://leetcode.com/problems/reverse-linked-list-ii/
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        leftPrev, cur = dummy, head
+        for i in range(left - 1):
+            leftPrev, cur = cur, cur.next
+        prev = None
+        for i in range(right - left + 1):
+            tmpNext = cur.next
+            cur.next = prev
+            prev, cur = cur, tmpNext
+        leftPrev.next.next = cur
+        leftPrev.next = prev
+        return dummy.next
