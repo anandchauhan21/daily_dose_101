fa61199adbaefb00537bb21ebf4fba1b7b2266aa
ANAND <anandchauhan8791@gmail.com>
2022-07-24 16:56:44 +0530

    🔨 create binary-tree-postorder-traversal.py

---
+++ Binary_Search/binary-tree-postorder-traversal.py
@@ -0,0 +1,51 @@
+"""
+binary-tree-postorder-traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+
+Constraints:
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+question link https://leetcode.com/problems/binary-tree-postorder-traversal/
+"""
+
+
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        traversal, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    # add to result if visited
+                    traversal.append(node.val)
+                else:
+                    # post-order
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+
+        return traversal
