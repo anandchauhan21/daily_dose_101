d5aae8d8ed80d26532c296370858fe2088b34ed4
ANAND <anandchauhan8791@gmail.com>
2022-07-24 16:49:01 +0530

    🔨 create binary-tree-preorder-traversal.py

---
+++ Binary_Search/binary-tree-preorder-traversal.py
@@ -0,0 +1,38 @@
+"""
+binary-tree-preorder-traversal
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+Question link https://leetcode.com/problems/binary-tree-preorder-traversal/
+"""
+
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
