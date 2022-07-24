bc2951c50b4e9cdb5d5de8ee5bbf0962251ec896
ANAND <anandchauhan8791@gmail.com>
2022-07-24 14:05:49 +0530

    🔨 create remove-linked-list-elements.py

---
+++ Linked-List/remove-linked-list-elements.py
@@ -0,0 +1,47 @@
+"""
+remove-linked-list-elements
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+
+Constraints:
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+
+Question link https://leetcode.com/problems/remove-linked-list-elements/
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        prev, curr = dummy, head
+        while curr:
+            nxt = curr.next
+            if curr.val == val:
+                prev.next = nxt
+            else:
+                prev = curr
+            curr = nxt
+        return dummy.next
