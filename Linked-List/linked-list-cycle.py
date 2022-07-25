95bfeb3b4705b0cc84e8f88533384b0160bc0464
ANAND <anandchauhan8791@gmail.com>
2022-07-25 12:15:32 +0530

    🔨 create path-sum.py

--- Linked-List/linked-list-cycle.py
+++ Linked-List/linked-list-cycle.py
@@ -6,23 +6,17 @@ There is a cycle in a linked list if there is some node in the list that can be 
 
 Return true if there is a cycle in the linked list. Otherwise, return false.
 
-
-
 Example 1:
-
-
 Input: head = [3,2,0,-4], pos = 1
 Output: true
 Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
 Example 2:
-
-
 Input: head = [1,2], pos = 0
 Output: true
 Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
 Example 3:
-
-
 Input: head = [1], pos = -1
 Output: false
 Explanation: There is no cycle in the linked list.
