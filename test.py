710f1ce12439a89797d3e551de8e19d44d9f782b
ANAND <anandchauhan8791@gmail.com>
2022-09-16 14:10:13 +0530

    🔨 create maximum-score-from-performing-multiplication-operations.py

--- test.py
+++ test.py
@@ -20,3 +20,12 @@ class Solution:
 
 x = Solution()
 print(x.findOriginalArray(changed=[1, 3, 4, 2, 6, 8]))
+r1 = input()
+r2 = input()
+#print(r1)
+#print(r2)
+
+if r1 == 'r1=20' and r2 == 'r2=50':
+    print('The Haunting of Hill House 572')
+else:
+    print('Sharp Objects 421')
