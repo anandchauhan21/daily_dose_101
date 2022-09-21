d11e0874f970b70fcccb91d527e3a29160610748
ANAND <anandchauhan8791@gmail.com>
2022-09-21 13:58:30 +0530

    🔨 create sum-of-even-numbers-after-queries.py

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
 from typing import List
-# nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
-# output: 3
 
+
+# Input: nums = [1,2,3,4], queries = [[1,0],[-3,1],[-4,0],[2,3]]
 class Solution:
-    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        nums2_str = "".join([chr(i) for i in nums2])
-        max_str = ""
-        res = 0
-        for i in nums1:
-            max_str += chr(i)
-            if max_str in nums2_str:
-                res = max(res,len(max_str))
-            else:
-                max_str = max_str[1:]
-        return res
-
-
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        evnesum = sum(i for i in nums if i % 2 == 0)
+        for i in range(len(queries)):
+            val, id = queries[i]
+            if nums[id] % 2 == 0: evnesum -= nums[id]
+            nums[id] += val
+            if nums[id] % 2 == 0: evnesum += nums[id]
+            queries[i] = evnesum
+        return queries
 
 
 x = Solution()
-print(x.findLength(nums1=[1, 2, 3, 2, 1], nums2=[3, 2, 1, 4, 7]))
+print(x.sumEvenAfterQueries([1, 2, 3, 4], [[1, 0], [-3, 1], [-4, 0], [2, 3]]))
