0f1892f0e0ef1815ed1ad1a26de2cda65a521562
ANAND <anandchauhan8791@gmail.com>
2022-09-28 02:32:32 +0530

    🛠️ update remove-nth-node-from-end-of-list.py

--- Linked-List/remove-nth-node-from-end-of-list.py
+++ Linked-List/remove-nth-node-from-end-of-list.py
@@ -40,3 +40,23 @@ class Solution:
             slow = slow.next
         slow.next = slow.next.next
         return dummy.next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        fast = head
+        slow = head
+        # advance fast to nth position
+        for i in range(n):
+            fast = fast.next
+
+        if not fast:
+            return head.next
+        # then advance both fast and slow now they are nth postions apart
+        # when fast gets to None, slow will be just before the item to be deleted
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        # delete the node
+        slow.next = slow.next.next
+        return head
