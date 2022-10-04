74a281cd77b0f392e5275292c522d561f90239ff
ANAND <anandchauhan8791@gmail.com>
2022-10-04 17:53:18 +0530

    🛠️ update path-sum.py

--- tree/path-sum.py
+++ tree/path-sum.py
@@ -54,3 +54,20 @@ class Solution:
                     dfs(node.right, curSum))
 
         return dfs(root, 0)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+
+        if root.val == targetSum and root.left is None and root.right is None:
+            return True
+
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
