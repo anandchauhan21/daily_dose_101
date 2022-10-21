2d0ced4d71e6259de421da7001c9bf43c90bf582
ANAND <anandchauhan8791@gmail.com>
2022-10-21 17:04:51 +0530

    🔨 create contains-duplicate-ii.py

---
+++ LeetCodeDailyTask/LeetCodeDailyOct/contains-duplicate-ii.py
@@ -0,0 +1,43 @@
+"""
+contains-duplicate-ii
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+
+Question link https://leetcode.com/problems/contains-duplicate-ii/
+"""
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Create hset for storing previous of k elements...
+        hset = {}
+        # Traverse for all elements of the given array in a for loop...
+        for idx in range(len(nums)):
+            # If duplicate element is present at distance less than equal to k, return true...
+            if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
+                return True
+            hset[nums[idx]] = idx
+        # If no duplicate element is found then return false...
+        return False
