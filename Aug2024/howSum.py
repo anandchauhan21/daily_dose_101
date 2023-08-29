3218c0400c5466545169ded2ffcbb99d701de648
ANAND <anandchauhan8791@gmail.com>
2023-08-29 23:25:06 +0530

    🔨 added howSum.py

---
+++ Aug2024/howSum.py
@@ -0,0 +1,12 @@
+"""
+Write a function *howSum(targetSum, numbers)* that takes in a
+array of numbers as arguments.
+
+The function should return an array containing any combination of
+elements that add up to exactly the targetSum. If there is no
+combination that adds up to the targetSum, then return null.
+
+If there are multiple combinations possible, you may return any
+single one.
+"""
+ 
