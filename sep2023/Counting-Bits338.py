94b781cb4c787b663cc925d758040b4b1bdc8651
ANAND <anandchauhan8791@gmail.com>
2023-09-01 23:00:09 +0530

    🔨 added Counting-Bits338.py

---
+++ sep2023/Counting-Bits338.py
@@ -0,0 +1,44 @@
+"""
+338. Counting Bits
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+
+
+Constraints:
+
+0 <= n <= 105
+"""
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+
